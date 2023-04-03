0562c7ae60ef7e6086720c7d3aa87dc704c04c0d
Ash Uppal <ashuppal@gmail.com>
2023-04-02 19:03:52 -0700

    added tests

--- src/Context/Settings/Settings.test.jsx
+++ src/Context/Settings/Settings.test.jsx
@@ -4,6 +4,27 @@ import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import SettingsProvider, { SettingsContext } from '.';
 
+
+describe('Settings Context', () => {
+  test('initial state loads as expected', () => {
+    render(
+      //add boundry to test
+      <SettingsProvider>
+        <SettingsContext.Consumer>
+          {(context) => {
+            expect(context.showCompleted).toBe(false);
+            expect(context.pageItems).toBe(3);
+            expect(context.sort).toBe('difficulty');
+          }
+          }
+        </SettingsContext.Consumer>
+      </SettingsProvider>
+    );
+  });
+});
+
+  
+
 describe('Settings Context', () => {
   test('initial state loads as expected', () => {
     render(
